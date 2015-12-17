b43439cf24c01facadad6ae56292898f6fbde852
qbodart <bodqu@LT7SLSL01.cargolux.com>
2015-12-17 16:23:06 +0100

    Minor changes
    Login and lost password design changes

---
+++ Source/Euro2016/src/Application/Sonata/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Application\Sonata\UserBundle;
+
+use SonataUserBundle\Controller\RegistrationController as BaseController;
+
+/**
+ *
+ */
+class RegistrationController extends BaseController
+{
+
+
+
+}
